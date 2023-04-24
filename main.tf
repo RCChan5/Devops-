dfed47011330c13a80d1e50175f94f70ac98c783
RCChan5 <43540251+RCChan5@users.noreply.github.com>
2023-04-23 20:40:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,41 +98,41 @@ resource "azurerm_cosmosdb_account" "db" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "example" {
-  name     = "tfex-azureml-${random_integer.ri.result}"
-  location = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-ml"
-}
+# resource "azurerm_resource_group" "example" {
+#   name     = "tfex-azureml-${random_integer.ri.result}"
+#   location = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-ml"
+# }
 
 resource "azurerm_application_insights" "example" {
   name                = "workspace-example-ai"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
 }
 
 resource "azurerm_key_vault" "example" {
   name                = "workspaceexamplekeyvault"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "premium"
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "workspacestorageaccount"
-  location                 = azurerm_resource_group.rg.location
-  resource_group_name      = azurerm_resource_group.rg.name
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "example" {
+#   name                     = "workspacestorageaccount"
+#   location                 = azurerm_resource_group.rg.location
+#   resource_group_name      = azurerm_resource_group.rg.name
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
 
 resource "azurerm_machine_learning_workspace" "example" {
   name                    = "example-workspace"
-  location                = azurerm_resource_group.example.location
-  resource_group_name     = azurerm_resource_group.example.name
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
   application_insights_id = azurerm_application_insights.example.id
   key_vault_id            = azurerm_key_vault.example.id
-  storage_account_id      = azurerm_storage_account.example.id
+  storage_account_id      = azurerm_storage_account.storage.id
 
   identity {
     type = "SystemAssigned"
